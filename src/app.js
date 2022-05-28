279069d417888fc7be8d9bd632c5d97a7eed93d4
Marcin Czekajski <czekajskimarcin@gmail.com>
2022-05-28 18:29:08 +0200

    refactor app structure

--- src/app.js
+++ src/app.js
@@ -1,9 +1,28 @@
 const express = require('express');
+
 const app = express();
+
+/**
+ * @swagger
+ * /cars:
+ *  get:
+ *    description: Get all cars list
+ *    responses:
+ *      200:
+ *      description: Success
+ */
+app.get('/cars', (req, res) => {
+    res.send([
+        {
+            id: 1,
+            brand: 'Toyota',
+            model: 'Corolla'
+        }
+    ])
+})
 
 app.get('/', (req, res) => {
     res.send('Car dealer api is working on port 3000')
 });
 
 module.exports = app;
-
--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 const app = require("./app");
+const swaggerJsDoc = require('swagger-jsdoc');
+
+const swaggerOptions = {
+    swaggerDefinition: {
+      info: {
+        title: 'Cars API',
+        version: '1.0.0'
+      }
+    },
+    apis: ['src/app.js']
+  }
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+console.log(swaggerDocs);
 
 const port = process.env.PORT || 3000;
 
