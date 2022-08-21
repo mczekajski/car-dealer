1131ccee5dfa4bb05270234ffa4c0477f617f63e
Marcin Czekajski <czekajskimarcin@gmail.com>
2022-08-21 21:17:14 +0200

    login validation fix

--- src/validation.js
+++ src/validation.js
@@ -13,8 +13,8 @@ const registerValidation = (data) => {
 // Login validation
 const loginValidation = (data) => {
   const schema = Joi.object({
-    email: Joi.string().min(6).email().required(),
-    password: Joi.string().min(8).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
   });
   return schema.validate(data);
 };
